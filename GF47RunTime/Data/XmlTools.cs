16b0b6f7551778579609f7734389b4404ab191f5
GF47 <liougourenmo@gmail.com>
2017-01-24 15:04:05 +0800

    优化了XmlTools中的一些方法

--- GF47RunTime/Data/XmlTools.cs
+++ GF47RunTime/Data/XmlTools.cs
@@ -1,40 +1,43 @@
-﻿using System.Xml;
+﻿using System;
+using System.Xml;
 
 namespace GF47RunTime.Data
 {
     public static class XmlTools
     {
+        public delegate bool TryParse<T>(string text, ref T value);
+
         public static XmlNode GetRootNode(string path)
         {
             XmlDocument doc = new XmlDocument();
             doc.Load(path);
             return doc.LastChild;
         }
-        public static bool GetInnerText(ref string text, XmlNode node)
+        public static bool HasInnerText(this XmlNode node, ref string text)
         {
             if (node == null) return false;
 
             text = node.InnerText;
             return true;
         }
-        public static bool GetInnerTextToInteger(ref int number, XmlNode node)
+        public static bool CanGetInnerTextAsInteger(this XmlNode node, ref int value)
         {
             string text = string.Empty;
-            if (GetInnerText(ref text, node))
+            if (node.HasInnerText(ref text))
             {
-                if (int.TryParse(text, out number))
+                if (int.TryParse(text, out value))
                 {
                     return true;
                 }
             }
             return false;
         }
-        public static bool GetInnerTextToFloat(ref float number, XmlNode node)
+        public static bool CanGetInnerTextAsFloat(this XmlNode node, ref float value)
         {
             string text = string.Empty;
-            if (GetInnerText(ref text, node))
+            if (node.HasInnerText(ref text))
             {
-                if (float.TryParse(text, out number))
+                if (float.TryParse(text, out value))
                 {
                     return true;
                 }
@@ -42,10 +45,10 @@ namespace GF47RunTime.Data
             return false;
         }
 
-        public static bool GetInnerTextToBool(ref bool value, XmlNode node)
+        public static bool CanGetInnerTextAsBool(this XmlNode node, ref bool value)
         {
             string text = string.Empty;
-            if (GetInnerText(ref text, node))
+            if (node.HasInnerText(ref text))
             {
                 if (bool.TryParse(text, out value))
                 {
@@ -55,33 +58,46 @@ namespace GF47RunTime.Data
             return false;
         }
 
-        public static bool GetAttributeValue(ref string value, string name, XmlNode node)
+        public static bool CanGetInnerTextAs<T>(this XmlNode node, ref T value, TryParse<T> tryParse)
+        {
+            string text = string.Empty;
+            if (node.HasInnerText(ref text))
+            {
+                if (tryParse(text, ref value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool HasAttribute(this XmlNode node, string attributeName, ref string value)
         {
             if (node == null) return false;
             if (node.Attributes == null) return false;
-            XmlAttribute attribute = node.Attributes[name];
+            XmlAttribute attribute = node.Attributes[attributeName];
             if (attribute == null) return false;
             value = attribute.Value;
             return true;
         }
-        public static bool GetAttributeValueToInteger(ref int number, string name, XmlNode node)
+        public static bool CanGetAttributeValueAsInteger(this XmlNode node, string attributeName, ref int value)
         {
             string text = string.Empty;
-            if (GetAttributeValue(ref text, name, node))
+            if (node.HasAttribute(attributeName, ref text))
             {
-                if (int.TryParse(text, out number))
+                if (int.TryParse(text, out value))
                 {
                     return true;
                 }
             }
             return false;
         }
-        public static bool GetAttributeValueToFloat(ref float number, string name, XmlNode node)
+        public static bool CanGetAttributeValueAsFloat(this XmlNode node, string attributeName, ref float value)
         {
             string text = string.Empty;
-            if (GetAttributeValue(ref text, name, node))
+            if (node.HasAttribute(attributeName, ref text))
             {
-                if (float.TryParse(text, out number))
+                if (float.TryParse(text, out value))
                 {
                     return true;
                 }
@@ -89,10 +105,10 @@ namespace GF47RunTime.Data
             return false;
         }
 
-        public static bool GetAttributeValueToBool(ref bool value, string name, XmlNode node)
+        public static bool CanGetAttributeValueAsBool(this XmlNode node, string attributeName, ref bool value)
         {
             string text = string.Empty;
-            if (GetAttributeValue(ref text, name, node))
+            if (node.HasAttribute(attributeName, ref text))
             {
                 if (bool.TryParse(text, out value))
                 {
@@ -101,7 +117,21 @@ namespace GF47RunTime.Data
             }
             return false;
         }
-        public static XmlNode GetXmlNodeByAttribute(XmlNodeList list, string attributeName, string value)
+
+        public static bool CanGetAttributeValueAs<T>(this XmlNode node, string attributeName, ref T value, TryParse<T> tryParse)
+        {
+            string text = string.Empty;
+            if (node.HasAttribute(attributeName, ref text))
+            {
+                if (tryParse(text, ref value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static XmlNode GetXmlNodeByAttribute(this XmlNodeList list, string attributeName, string value)
         {
             if (list == null || list.Count <= 0) { return null; }
 
@@ -117,6 +147,5 @@ namespace GF47RunTime.Data
             }
             return null;
         }
-
     }
 }
